399c5c1c3738630ab2d4b80549ea8bf463233f3f
iman <iman@bitmascot.com>
2017-07-26 09:54:07 +0600

    init

---
+++ IntroductionToCsharp/ProjectA.TeamA/ClasssA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectA.TeamA
+{
+    public class ClasssA
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Team A Print method");
+        }
+    }
+}
