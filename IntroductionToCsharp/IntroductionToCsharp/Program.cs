73ca4734b4da24240aecffaa816e6eaa47ae983c
iman <iman@bitmascot.com>
2017-07-27 19:13:30 +0600

    fdfd

--- IntroductionToCsharp/IntroductionToCsharp/Program.cs
+++ IntroductionToCsharp/IntroductionToCsharp/Program.cs
@@ -6,13 +6,13 @@ public class Program
     static void Main()
     {
         try {
-            StreamReader streamReader = new StreamReader(@"C:\csharp\csharphub\TestingFiles\data2.txt");
+            StreamReader streamReader = new StreamReader(@"C:\csharp\csharphub\TestingFiles\data.txt");
             Console.WriteLine(streamReader.ReadToEnd());
             streamReader.Close();
         }
         catch(Exception ex)
         {
-
+            Console.WriteLine(ex.Message);
         }        
     }
 
