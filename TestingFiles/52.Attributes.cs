43622d702c92f97919aca26f777199b0010cc5af
iman <iman@bitmascot.com>
2017-07-30 17:03:53 +0600

    aa

---
+++ TestingFiles/52.Attributes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntroductionToCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum = Calculator.Add(10, 20);
+            //int Sum = Calculator.Add(new List<int>() { 10, 20, 30});
+
+            Console.WriteLine("Sum {0}", Sum);
+        }
+
+    }
+}
+
+public class Calculator
+{
+    [Obsolete("Use Add(List<int> Numbers) Method", false)]
+    public static int Add(int FirstNumber, int SecondNumber)
+    {
+        return FirstNumber + SecondNumber;
+    }
+
+    public static int Add(List<int> Numbers)
+    {
+        int Sum = 0;
+        foreach(int Number in Numbers)
+        {
+            Sum = Sum + Number;
+        }
+        return Sum;
+    }
+
+}
